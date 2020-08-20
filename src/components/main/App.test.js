58bbc3f2e942216ce8480b58158c8ebd30eb16fd
raudelgar <raudel24@gmail.com>
2020-08-20 15:07:37 -0400

    fixing test

--- src/components/main/App.test.js
+++ src/components/main/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import { findByDataTest } from '../../setupTests.js';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	// const { getByText } = render(<App />);
+	const wrapper = shallow(<App />);
+	// const linkElement = getByText(/learn react/i);
+	expect(findByDataTest(wrapper, 'react-main').text()).toMatch(/learn react/i);
+	// expect(linkElement).toBeInTheDocument();
 });
